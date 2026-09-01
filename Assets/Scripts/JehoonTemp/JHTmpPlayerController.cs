e81004d2d920ea5537149353f2341e94dee20f76
eytg8e <37997297+eytg8e@users.noreply.github.com>
2026-09-01 11:14:06 +0900

    체력, 빵 값에 맞춰서 수치 나오는 UI
    체력, 빵 값을 받아서 수치나 칸으로 표현하는 부분까지 구현
    Todo - 피격, 던지는 등 수치가 변하는 이벤트 발생 시 수치가 변하는 부분

---
+++ Assets/Scripts/JehoonTemp/JHTmpPlayerController.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class JHTmpPlayerController : MonoBehaviour
+{
+    [Tooltip("카메라")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private CamController camController;
+    [Header("제훈 임시 플레이어 무기 관리자(?)")]
+    public JHTmpWeaponHandler JHTmpWeaponHandler;
+    [Header("플레이어 조작")]
+    [SerializeField] Rigidbody pRigid;
+    [Tooltip("W A S D")]
+    public InputAction moveInput;
+    [Tooltip("Space")]
+    public InputAction jumpInput;
+    [Tooltip("근접공격 애니메이션")]
+    public Animator meleeAni;
+    public bool isThrowReady = false;
+    /*
+    [Tooltip("마우스 좌 우")]
+    public InputAction mouseInput;
+    */
+    [SerializeField] private float rightClickTime = 0f;
+    [Tooltip("조준을 위한 우클릭 유지 시간")]
+    [SerializeField] private float aimTime = 1.5f;
+
+    public bool isDead = false;
+    [SerializeField] float walkSpeed;
+    [Tooltip("마우스 좌 우 회전 속도(카메라 회전 속도)")]
+    [SerializeField] float rotateSpeed;
+    [Tooltip("플레이어 이동 방향")]
+    [SerializeField] Vector2 movePos;
+    [SerializeField] float jumpHeight;
+
+    private void Awake(){
+        pRigid = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        InitInputAction();
+    }
+
+    /// <summary>
+    /// InputAction 활성화
+    /// </summary>
+    void InitInputAction()
+    {
+        moveInput.Enable();
+        jumpInput.Enable();
+        //mouseInput.Enable();
+    }
+
+    void Update(){
+        CheckKeyboardInput();
+        RotatePlayer();
+    }
+
+    /// <summary>
+    ///  InputAction 감지
+    /// </summary>
+    void CheckKeyboardInput()
+    {
+        //사망 시 입력 무시
+        if (isDead) 
+            return;
+        MovePlayer();
+        JumpPlayer();
+        AttackPlayer();
+    }
+
+    #region 플레이어 조작(회전, 이동, 회전, 공격)
+
+    /// <summary>
+    /// 마우스 화전에 따른 플레이어 회전, 추후 카메라 회전과 연동으로 변경 필요
+    /// </summary>
+    void RotatePlayer()
+    {
+        float mouseXInput = Mouse.current.delta.x.ReadValue() * rotateSpeed * Time.deltaTime;
+        //플레이어 회전
+        transform.Rotate(Vector3.up * mouseXInput);
+    }
+
+    /// <summary>
+    /// 플레이어 이동
+    /// </summary>
+    void MovePlayer()
+    {
+        movePos = moveInput.ReadValue<Vector2>();
+
+        if (movePos.sqrMagnitude > 0.001f)
+        {
+            //카메라 시선 방향 확인
+            Vector3 camForward = cameraTransform.forward;
+            Vector3 camRight = cameraTransform.right;
+
+            camForward.y = 0f;
+            camRight.y = 0f;
+
+            camForward.Normalize();
+            camRight.Normalize();
+
+            //플레이어 이동 위치 설정 및 이동
+            Vector3 movementDirection = (camForward * movePos.y) + (camRight * movePos.x);
+            transform.Translate(movementDirection * Time.deltaTime * walkSpeed, Space.World);
+        }
+    }
+
+    /// <summary>
+    /// 플레이어 점프
+    /// </summary>
+    void JumpPlayer()
+    {
+        if(jumpInput.triggered)
+            pRigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+    }
+
+    /// <summary>
+    /// 플레이어 공격
+    /// </summary>
+    void AttackPlayer()
+    {
+        //좌 "클릭"
+        if (Input.GetMouseButtonDown(0)){
+            if(isThrowReady)
+                return;
+            JHTmpWeaponHandler.MeleeAttack();
+            meleeAni.Play("SwingDiagonal");
+        }
+        //우 "클릭"
+        else if (Input.GetMouseButton(1))
+        {
+            rightClickTime += Time.deltaTime;
+            if (rightClickTime >= aimTime){
+                isThrowReady = true;
+                camController.CameraAim(true);
+            }
+            
+        }
+        // 우클릭 해제 시 카메라 줌아웃
+        else if (Input.GetMouseButtonUp(1))
+        {
+            JHTmpWeaponHandler.ThrowBread();
+            rightClickTime = 0f;
+            isThrowReady = false;
+        }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 충돌 감지
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
---
+++ Assets/Scripts/JehoonTemp/JHTmpWeaponHandler.cs
@@ -0,0 +1,128 @@
+using UnityEngine;
+using System.Collections;
+
+public class JHTmpWeaponHandler : MonoBehaviour
+{
+    [SerializeField] private CamController camController;
+    [SerializeField] private Animator weaponHandlerAni;
+    [Header("무기 설정")]
+    [Tooltip("무기 프리팹(바게트 빵)")]
+    public Baguette BreadPrefs;
+    [Tooltip("최대 빵 보유 갯수")]
+    [SerializeField] private int MaxBread = 5;
+    [Tooltip("현재 빵 보유 횟수, 자동으로 초기화")]
+    [SerializeField] private int curBread;
+    [Header("원거리 공격 정보")]
+    [Tooltip("쿨타임")]
+    [SerializeField] private float throwCooldownTime;
+    [Tooltip("쿨타임 여부")]
+    [SerializeField] private bool isCooldown = false;
+    [Tooltip("빵 던지는 힘(속도)")]
+    [SerializeField] private float throwForce = 1f;
+    [Tooltip("빵 재장전 시간")]
+    [SerializeField] private float reloadTime = 3f;
+
+
+    //시작 시점에 빵 갯수 초기화  
+    void Start()
+    {
+        curBread = MaxBread;
+    }
+
+#region 빵 사용 관련
+    void UseBread()
+    {
+        if (curBread > 0)
+        {
+            curBread--;
+            Instantiate(BreadPrefs, transform.position + transform.forward * 1.5f, transform.rotation);
+        }
+    }
+
+    public void ReloadBread()
+    {
+        curBread = MaxBread;
+    }
+
+    public void UpgradeMaxBread(int amount)
+    {
+        MaxBread += amount;
+        curBread = MaxBread;
+    }
+#endregion
+
+#region 빵 공격 관련
+    /// <summary>
+    /// 근접 공격
+    /// </summary>
+    public void MeleeAttack()
+    {
+        Debug.Log("빵 휘두르기! (플레이어가 애니메이션 진행");
+    }
+
+    /// <summary>
+    /// 빵 던지기
+    /// </summary>
+    public void ThrowBread()
+    {
+        if (isCooldown){
+            Debug.Log("아직 쿨타임이 남았습니다.");
+            return;
+        }
+        else
+        {
+            Debug.Log("빵 던지기!");
+            //curBread--;
+            isCooldown = true;
+            //빵 던지기
+            BreadPrefs.ThrowBaguette(throwForce);
+            //빵 재장전
+            // weaponHandlerAni.Play("ReloadBaguette");
+            //시간 측정
+            StartCoroutine(ThrowCooldown());
+            StartCoroutine(ThrowBreadCoroutine());
+        }
+    }
+
+#endregion
+
+#region 빵 개수 관련
+    public int GetCurrentBread()
+    {
+        return curBread;
+    }
+
+    public int GetMaxBread()
+    {
+        return MaxBread;
+    }
+
+#endregion
+
+    /// <summary>
+    /// 던지기 쿨타임 코루틴
+    /// </summary>
+    IEnumerator ThrowCooldown()
+    {
+
+        float curCoolTime = 0;
+        while (curCoolTime < throwCooldownTime){
+            curCoolTime++;
+            yield return new WaitForSeconds(1f);
+        }
+        isCooldown = false;
+    }
+
+    /// <summary>
+    /// 조준 상태 유지 코루틴(재장전 시간 연동)
+    /// </summary>
+    IEnumerator ThrowBreadCoroutine()
+    {
+        float curReloadTime = 0;
+        while (curReloadTime < reloadTime){
+            curReloadTime++;
+            yield return new WaitForSeconds(1f);
+        }
+        camController.CameraAim(false);
+    }
+}
