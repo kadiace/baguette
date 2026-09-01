a8b2092e1f09e6fd2eb2da5a1ffdfde7668ce3a5
HeoYoon <yoony4175@gmail.com>
2026-09-01 22:11:22 +0900

    Get house object from scene.

--- Assets/Scripts/Scenes/GameSceneContext.cs
+++ Assets/Scripts/Scenes/GameSceneContext.cs
@@ -22,13 +22,33 @@ public class rlacjfghks95_GameSceneContext : MonoBehaviour
             Define.HouseColor color = Managers.Deliver.GenerateDeliveryCard();
             // 이름 하나하나 찾아서 game object 를 변환하는 노가다를 해야하나?
             // house (특히 loop) 의 game object 리스트를 가져온다
+            GameObject houses = GameObject.Find("Houses");
+            int count = houses.transform.childCount;
+
+            List<GameObject> houseList = new List<GameObject>();
+
+            for (int i = 0; i < count; i++)
+            {
+                Transform child = houses.transform.GetChild(i);
+                houseList.Add(child.gameObject);
+            }
+            
             // 많은 house 들 중에서 랜덤으로 하나 선택
+            GameObject selectedHouse = houseList[Random.Range(0, houseList.Count)];
+
+            // 선택된 house 의 지붕, 주민을 선택
+            Transform selectedRoof = selectedHouse.transform.Find("Roof");
+            Transform selectedVillager = selectedHouse.transform.Find("Villager");
+
             // House 지붕에 color 적용
             // 참고: Define.HouseColors.Colors[color] 로 Color32, 컬러 코드 값 추출 가능
-            // house 하나 랜덤으로 선택
-            // 2번 줄에서 추출한 color 값으로 house 지붕 색상 변경
-            // 해당 house 앞에 배달 완료 트리거가 될 npc 생성
-            // npc 주변에 트리거 오브젝트 생성
+            
+            Renderer renderer = selectedRoof.GetComponent<Renderer>();
+            renderer.material.color = Define.HouseColors.Colors[color];
+
+            // npc 주변에 트리거 오브젝트 활성화
+            GameObject trigger = selectedVillager.Find("Trigger_Range").gameObject;
+            trigger.SetActive(true);
         }
     }
 }
