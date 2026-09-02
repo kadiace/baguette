742235d290d61bcff02b97c65c673bc2c9e644c8
kadiace <rlacjfghks95@gmail.com>
2026-09-02 20:48:34 +0900

    Refactor Deliver manager.

--- Assets/Scripts/Scenes/GameSceneContext.cs
+++ Assets/Scripts/Scenes/GameSceneContext.cs
@@ -36,52 +36,30 @@ public class GameSceneContext : MonoBehaviour
 
     private void GenerateNewDeliveryCard()
     {
-        UI_DeliveryCard deliveryCard = Managers.Deliver.GenerateDeliveryCard();
-        if (deliveryCard != null)
+        if (Managers.Deliver.IsFull())
+            return;
+
+        // 이름 하나하나 찾아서 game object 를 변환하는 노가다를 해야하나?
+        // house (특히 loop) 의 game object 리스트를 가져온다
+        GameObject houses = GameObject.Find("Houses");
+        int count = houses.transform.childCount;
+
+        List<GameObject> houseList = new List<GameObject>();
+
+        for (int i = 0; i < count; i++)
         {
-            // 이름 하나하나 찾아서 game object 를 변환하는 노가다를 해야하나?
-            // house (특히 loop) 의 game object 리스트를 가져온다
-            GameObject houses = GameObject.Find("Houses");
-            int count = houses.transform.childCount;
-
-            List<GameObject> houseList = new List<GameObject>();
-
-            for (int i = 0; i < count; i++)
-            {
-                Transform child = houses.transform.GetChild(i);
-                houseList.Add(child.gameObject);
-            }
-
-            // 많은 house 들 중에서 랜덤으로 하나 선택
-            GameObject selectedHouse = houseList[UnityEngine.Random.Range(0, houseList.Count)];
-            while (Managers.Deliver.IsHouseDuplicated(selectedHouse))
-            {
-                selectedHouse = houseList[UnityEngine.Random.Range(0, houseList.Count)];
-            }
-
-            // 선택된 house 의 지붕, 주민을 선택
-            Transform selectedRoof = selectedHouse.transform.Find("Roof");
-            Transform selectedVillager = selectedHouse.transform.Find("Villager");
-
-            // House 지붕에 color 적용
-            // 참고: Define.HouseColors.Colors[color] 로 Color32, 컬러 코드 값 추출 가능
-            Renderer[] renderers = selectedRoof.GetComponentsInChildren<Renderer>();
-            Color originRoofColor = renderers[0].material.color;
-            foreach (Renderer renderer in renderers)
-            {
-                renderer.material.color = Define.HouseColors.Colors[deliveryCard.Color];
-            }
-
-            // npc 주변에 트리거 오브젝트 활성화
-            GameObject trigger = selectedVillager.Find("Trigger").gameObject;
-            trigger.SetActive(true);
-
-            // trigger enter 후 처리를 위해 정보 저장
-            VillagerInteractionController villagerInteractionController = trigger.GetorAddComponent<VillagerInteractionController>();
-            villagerInteractionController.OriginHouseColor = originRoofColor;
-            villagerInteractionController.DeliveryCard = deliveryCard;
-            villagerInteractionController.Roof = selectedRoof.gameObject;
+            Transform child = houses.transform.GetChild(i);
+            houseList.Add(child.gameObject);
         }
+
+        // 많은 house 들 중에서 랜덤으로 하나 선택
+        GameObject selectedHouse = houseList[UnityEngine.Random.Range(0, houseList.Count)];
+        while (Managers.Deliver.IsHouseDuplicated(selectedHouse))
+        {
+            selectedHouse = houseList[UnityEngine.Random.Range(0, houseList.Count)];
+        }
+
+        Managers.Deliver.GenerateDeliveryCard(selectedHouse);
     }
 
     private void SpawnTraffic()
--- Assets/Scripts/UIs/UI_DeliveryCard.cs
+++ Assets/Scripts/UIs/UI_DeliveryCard.cs
@@ -21,11 +21,16 @@ public class UI_DeliveryCard : UI_Base
     private int _reward;
     private int _quantity;
     private float _time;
+    private Color _originHouseColor;
+    private GameObject _roof;
+
 
     public GameObject House { get { return _house; } set { _house = value; } }
     public Define.HouseColor Color { get { return _color; } }
     public int Reward { get { return _reward; } }
     public int Quantity { get { return _quantity; } }
+    public Color OriginHouseColor { set { _originHouseColor = value; } }
+    public GameObject Roof { set { _roof = value; } }
 
     public override void Init()
     {
@@ -43,7 +48,7 @@ public class UI_DeliveryCard : UI_Base
 
         if (_time <= 0f)
         {
-            Managers.Deliver.DestroyDeliveryCard(this);
+            Managers.Deliver.DestroyDelivery(this);
             return;
         }
 
@@ -68,10 +73,21 @@ public class UI_DeliveryCard : UI_Base
         SetTime();
     }
 
+    public void DestroyCard()
+    {
+        Renderer[] renderers = _roof.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.color = _originHouseColor;
+        }
+
+        Managers.Resource.Destroy(gameObject);
+    }
+
     private void SetTime()
     {
         int minutes = Mathf.FloorToInt(_time / 60f);
         int seconds = Mathf.FloorToInt(_time % 60f);
-        GetText((int)Texts.Time).GetComponent<TextMeshProUGUI>().text = $"{minutes}:{seconds}";
+        GetText((int)Texts.Time).GetComponent<TextMeshProUGUI>().text = $"{minutes:D2}:{seconds:D2}";
     }
 }
