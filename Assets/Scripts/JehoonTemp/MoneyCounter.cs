2c4d25f7a50301ca1ec3637835747313ba84795f
eytg8e <37997297+eytg8e@users.noreply.github.com>
2026-09-02 21:45:32 +0900

    moneyUI(HUD) sync

--- Assets/Scripts/JehoonTemp/MoneyCounter.cs
+++ Assets/Scripts/JehoonTemp/MoneyCounter.cs
@@ -1,16 +1,12 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoneyCounter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        TextMeshProUGUI moneyUI = gameObject.GetorAddComponent<TextMeshProUGUI>();
+        Managers.Money.MoneyUI = moneyUI;
     }
 }
